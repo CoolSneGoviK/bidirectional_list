efe76567675074d02d337dd7368c4ed4663d5b1f
kaz_kolyan97@mail.ru <kaz_kolyan97@mail.ru>
2021-08-25 11:03:54 +0300

    добавил методы в List2: добавление элемента в начало добавление по индексу получение данных по индексу (метод) получение данных по индексу (перегруженный оператор) метод get_Size - получение колличества элементов в списке

--- bidirectional_list/bidirectional_list.cpp
+++ bidirectional_list/bidirectional_list.cpp
@@ -6,13 +6,23 @@ using namespace std;
 int main()
 {
 	List2<int> N;
-	N.add_element(1);
+	N.add_element(1); // добавление элемента в конец
 	N.add_element(2);
 	N.add_element(3);
 	N.add_element(4);
+	N.add_element(5);
+	N.add_element(6);
+	N.add_element(7);
 
-	N.show_start_end();
-	N.show_end_start();
+	//N.add_start(10); // добавление элемента в начало
+
+	//N.add_by_index(100, 3); // добавление элемента по индексу
+	
+	//cout << N.index_value(3) << endl; // получение значения элемента по индексу (метод)
+	//cout << N[3] << endl; // получение значения элемента по индексу (перегруженный оператор)
+
+	N.show_start_end(); // вывести все элементы списка с head
+	N.show_end_start(); // вывести все элементы списка с tail
 
 	system("pause");
 	return 0;
--- bidirectional_list/class2.h
+++ bidirectional_list/class2.h
@@ -10,12 +10,12 @@ public:
 	List2() {};
 	~List2() {};
 
-	void add_element(T data); // добавление в конец
-	void add_start(T data); // добавление в начало
-	void add_by_index(T data, int index = 0); // добавление по индексу
+	void add_element(T data = T()); // добавление в конец +
+	void add_start(T data = T()); // добавление в начало +
+	void add_by_index(T data = T(), int index = 0); // добавление по индексу, счет элементов начинается с 0 +
 
-	T& index_value(int index = 0); // получение значения элемента
-	T& operator [] (int index); // получение значения элемента
+	T& index_value(int index = 0); // получение значения элемента, счет элементов начинается с 0 (метод) +
+	T& operator [] (int index); // получение значения элемента, счет элементов начинается с 0 (перегруженный оператор) +
 
 	void show_start_end(); // вывод всех элементов списка с head (с головы) +
 	void show_end_start(); // вывод всех элементов списка с tail (с хвоста) +
@@ -26,6 +26,8 @@ public:
 	void delete_last(); // удаление последнего элемента
 	void delete_by_index(int index = 0); // удаление по индексу
 	void delete_all(); // удаление всех элементов списка
+
+	int get_Size() { return Size; }; // для получения колличества элементов
 
 private:
 	template <class T>
@@ -75,6 +77,121 @@ void List2<T>::add_element(T data)
 		head = head->next;
 	}
 }
+
+template <class T> // добавление элемента списка в начало
+void List2<T>::add_start(T data)
+{
+	if (Size == 0) 
+	{ // если список пустой
+		add_element(data);
+		return;
+	}
+
+	Node<T>* current = new Node<T>(data, nullptr, head);
+	head->previos = current;
+	head = current;
+	Size++;
+};
+
+template <class T> // добавление элементов по индексу, счет элементов начинается с 0
+void List2<T>::add_by_index(T data, int index)
+{
+	if (index == Size)
+	{
+		add_element(data);
+		return;
+	}
+
+	if (index > Size) 
+	{
+		cout << "error" << endl;
+		return;
+	}
+
+	if (index == 0) 
+	{
+		add_start(data);
+		return;
+	}
+
+	int counter = 0;
+	Node<T>* current = head;
+	Node<T>* new_node = nullptr; // новый узел
+
+	while (head) 
+	{ 
+		if (counter == index - 1) 
+		{ // нашли элемент за которым нужно вставить новый
+			new_node = new Node<T>(data, head, head->next); // передаем значение/указатель на предыдущий/указатель на следующий
+			head->next = new_node;
+			head->next->next->previos = new_node;
+			head = current;
+			break;
+		}
+		head = head->next;
+		counter++;
+	}
+};
+
+template <class T> // возвращает значение элемента по заданному индексу, считает от 0
+T& List2<T>::index_value(int index)
+{
+	if (index == 0) 
+	{
+		return head->data;
+	}
+
+	if (index > Size -1) 
+	{
+		cout << "error" << endl;
+		return head->data;
+	}
+
+	int counter = 0;
+	Node<T>* current = head;
+
+	while (head) 
+	{
+		if (counter == index) 
+		{
+			Node<T>* current2 = head;
+			head = current;
+			return current2->data;
+		}
+		counter++;
+		head = head->next;
+	}
+};
+
+template <class T> // возвращает значение элемента по заданному индексу, считает от 0
+T& List2<T>::operator[](int index)
+{
+	if (index == 0)
+	{
+		return head->data;
+	}
+
+	if (index > Size - 1)
+	{
+		cout << "error" << endl;
+		return head->data;
+	}
+
+	int counter = 0;
+	Node<T>* current = head;
+
+	while (head)
+	{
+		if (counter == index)
+		{
+			Node<T>* current2 = head;
+			head = current;
+			return current2->data;
+		}
+		counter++;
+		head = head->next;
+	}
+};
 
 template <class T> // вывод элементов с head
 void List2<T>::show_start_end()
