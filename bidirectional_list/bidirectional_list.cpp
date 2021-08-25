c5d13e010c4967623152b138951407b88847bb76
kaz_kolyan97@mail.ru <kaz_kolyan97@mail.ru>
2021-08-25 09:23:21 +0300

    добавил методы в List: добавление элемента в конец списка вывод элементов списка с головы вывод эдементов списка с хвоста

--- bidirectional_list/bidirectional_list.cpp
+++ bidirectional_list/bidirectional_list.cpp
@@ -7,6 +7,12 @@ int main()
 {
 	List2<int> N;
 	N.add_element(1);
+	N.add_element(2);
+	N.add_element(3);
+	N.add_element(4);
+
+	N.show_start_end();
+	N.show_end_start();
 
 	system("pause");
 	return 0;
--- bidirectional_list/class2.h
+++ bidirectional_list/class2.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <iostream>
+using namespace std;
+
 template <class T>
 class List2 
 {
@@ -14,7 +17,8 @@ public:
 	T& index_value(int index = 0); // получение значения элемента
 	T& operator [] (int index); // получение значения элемента
 
-	void show(); // вывод всех элементов списка
+	void show_start_end(); // вывод всех элементов списка с head (с головы) +
+	void show_end_start(); // вывод всех элементов списка с tail (с хвоста) +
 
 	void swap(int index1 = 0, int index2 = 0); // поменять местами значения в списке
 
@@ -28,7 +32,7 @@ private:
 	class Node 
 	{
 	public:
-		Node(T data = T(), Node<T>* next = nullptr, Node<T>* previos = nullptr)
+		Node(T data = T(), Node<T>* previos = nullptr, Node<T>* next = nullptr)
 		{
 			this->data = data;
 			this->next = next;
@@ -47,14 +51,67 @@ private:
 	int Size = 0;
 };
 
-template <class T>
+template <class T> // добавление элемента в конец списка
 void List2<T>::add_element(T data) 
 { 
-	if (head = nullptr) 
+	if (head == nullptr) 
 	{ // если список пуст
 		head = new Node<T>(data);
 		Size++;
 		return;
 	}
 
+	Node<T>* current = head;
+	while (head) 
+	{ // если не пуст // добавить приравнивание последнего элемент а tail и с указателями понять что как
+		if (head->next == nullptr)
+		{
+			tail = new Node<T>(data, head, nullptr);
+			head->next = tail;
+			head = current;
+			Size++;
+			break;
+		}
+		head = head->next;
+	}
 }
+
+template <class T> // вывод элементов с head
+void List2<T>::show_start_end()
+{
+	Node<T>* current = head;
+	while (head) 
+	{
+		cout << head->data << " ";
+		
+		if (head->next == nullptr)
+		{
+			cout << endl;
+			head = current;
+			break;
+		}
+		
+		head = head->next;
+	}
+};
+
+template <class T> // вывод элементов с tail
+void List2<T>::show_end_start() 
+{
+	Node<T>* current = tail;
+	while (tail) 
+	{
+		cout << tail->data << " ";
+		
+		if (tail->previos == nullptr) 
+		{
+			cout << endl;
+			tail = current;
+			break;
+		}
+		
+		tail = tail->previos;
+	}
+}
+
+
